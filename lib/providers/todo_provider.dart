6e653ce66d35db6d9b73b84327513da7305f95eb
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2026-06-17 02:06:12 -0700

    feat:implemented todo_provider

---
+++ lib/providers/todo_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:provider_todo_list/models/todo_model.dart';
+
+class TodoProvider with ChangeNotifier {
+final List<TodoItemModel> _todos=[];
+ List<TodoItemModel>  get todos=>_todos;
+
+ void addTodo(String title){
+ if(title.trim().isEmpty) return;
+ final newTodo=TodoItemModel(
+  id: DateTime.now().millisecondsSinceEpoch.toString(),
+   title: title.trim()
+   );
+   _todos.add(newTodo);
+   notifyListeners();
+ }
+}
