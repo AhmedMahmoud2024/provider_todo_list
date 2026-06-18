1319c6597a0a5505fdd3762c98bcf7668a18b466
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2026-06-18 08:50:23 -0700

    feat:injected the bloc on top of MaterialApp at main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_todo_list/data/providers/task_provider.dart';
+import 'package:provider_todo_list/presentation/bloc/task_cubit.dart';
 import 'package:provider_todo_list/presentation/screens/task_list_screen.dart';
 
 void main() {
@@ -13,8 +15,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-        create: (context)=>TaskProvider(),
+    return BlocProvider(
+        create: (context)=>TaskCubit(),
       child: MaterialApp(
         title: 'Task Manager',
         debugShowCheckedModeBanner: false,
