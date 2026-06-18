0f9a9a543138cb1ee50b7845790cf7a793ce989a
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2026-06-17 17:40:59 -0700

    feat:implemented onToggle ui for task_item

--- lib/presentation/widgets/task_item.dart
+++ lib/presentation/widgets/task_item.dart
@@ -3,12 +3,14 @@ import 'package:provider_todo_list/data/models/task_model.dart';
 class TodoItem extends StatelessWidget{
   final TaskModel task;
   final VoidCallback onDelete;
+   final VoidCallback onToggle;
   final Function(String) onEdit;
   const TodoItem({
   super.key,
   required this.task,
   required this.onEdit,
-  required this.onDelete
+  required this.onToggle,
+  required this.onDelete,
   });
   
   @override
@@ -16,15 +18,35 @@ class TodoItem extends StatelessWidget{
     final textController=TextEditingController(text: task.title);
     return AnimatedContainer(
     duration: const Duration(milliseconds: 200),
+    curve: Curves.bounceIn,
         margin: EdgeInsets.symmetric(vertical: 4,horizontal: 16),
         decoration: BoxDecoration(
           // ignore: deprecated_member_use
-          color: Colors.grey.withOpacity(0.2)
+          color: task.isDone?Colors.deepPurple.withOpacity(0.05)
+          : Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.04),
+              blurRadius: 6,
+              offset: const Offset(0, 2)
+            )
+          ]
         ),
         child: ListTile(
-         
+             leading: Checkbox
+             (value: task.isDone,
+              onChanged: (_)=>onToggle(),
+              activeColor: Colors.deepPurple,
+              ),         
                  title: Text(
                     task.title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      decoration: task.isDone? TextDecoration.lineThrough :null,
+                      color: task.isDone? Colors.grey:Colors.black87,
+                    ),
                  ),
                trailing: Row(
                 mainAxisSize: MainAxisSize.min,
