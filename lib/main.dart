c8d0e7604fa1d3fcd939ce21095874c78d2b7673
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2026-06-19 06:37:33 -0700

    feat:refactoring state management with riverpod & implemented  task_notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider_todo_list/presentation/bloc/task_cubit.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:provider_todo_list/presentation/screens/task_list_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( //wrap with providerScope
+     ProviderScope(
+      child: MyApp()
+      )
+     );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,16 +16,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-        create: (context)=>TaskCubit(),
-      child: MaterialApp(
-        title: 'Task Manager',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        ),
-        home: TaskListScreen()
+    return MaterialApp(
+      title: 'Task Manager',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      home: TaskListScreen()
     );
   }
 }
